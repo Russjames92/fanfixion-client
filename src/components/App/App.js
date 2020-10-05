91038197defaad5d4c8c6167ed1e943a27d14851
Russ <18russjames@gmail.com>
2020-10-05 10:59:13 -0700

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {}
+
+  // static //error function
+  // {}
+
+  render() {
+    return (
+      <div className='App'>
+        <p>Hello World!</p>
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/services/episode-api-service.js
@@ -0,0 +1,29 @@
+import config from '../config'
+import TokenService from '.token-service'
+
+const EpisodeApiService = {
+    getEpisodes() {
+        return fetch(`${config.API_ENDPOINT}/episodes`, {
+            headers: {},
+        })
+            .then(res =>
+                (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+                )
+    },
+    getEpisode(episodeId) {
+        return fetch(`${config.API_ENDPOINT}/episodes/${episodeId}`, {
+            headers: {
+                'authorization': `bearer ${TokenService.getAuthToken()}`,
+            },
+        })
+            .then(res =>
+                (!res.ok)
+                ? res.json().then(e => Promise.reject(e))
+                : res.json()
+                )
+    },
+}
+
+export default EpisodeApiService
