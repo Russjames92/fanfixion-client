ed93ea85b8893fa95960edf7227e43bc4762bc84
Russ <18russjames@gmail.com>
2020-10-16 10:27:17 -0700

    ready to final deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://shrouded-savannah-61276.herokuapp.com/api',
     TOKEN_KEY: 'fanfixion-client-auth-token',
 }
-
-
-// API_ENDPOINT: 'https://shrouded-savannah-61276.herokuapp.com/api',
